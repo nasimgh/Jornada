c6838e892299efcf715ab4b1112f17ebb9b91c66
nasimgh <n-ghazan@nmsu.edu>
2015-07-20 14:24:04 -0600

    statistics will be updated by filters

--- AdminDashboard/PlotStats/helpers.R
+++ AdminDashboard/PlotStats/helpers.R
@@ -32,6 +32,11 @@ req <- GET("https://silicon-bivouac-496.appspot.com/_ah/api/plotendpoint/v1/plot
 stop_for_status(req)
 json_data<-content(req,as = "text")
 
+statMean <<- 0
+statMedian <<- 0
+statMin <<- 0
+statMax <<- 0
+
 ### reading from static file
 # json_file <- "data/plotList.json"
 # json_data <- readChar(json_file, file.info(json_file)$size)
@@ -108,6 +113,36 @@ getUserData<-function (userList,dates,count){
   return (data)
 }
 
+updateStats<- function (userList,dates,count){
+   
+  statMean <<- 0
+  statMedian <<- 0
+  statMin <<- 0
+  statMax <<- 0
+  
+  start <- dates[1]
+  end <- dates[2]
+  
+  min <- count[1]
+  max <- count[2]
+  
+  userData <-json_data[json_data$modifiedDate <= end & json_data$modifiedDate >= start,]
+  userData <- userData[userData$recorderName  %in% userList ,]
+  
+  if(nrow(userData) ==0 ) return (NULL)
+  
+  userData <- aggregate(userData$value , list(user = userData$recorderName, time = userData$shortdate), sum)
+  
+  userData <- userData[userData$x <= max & userData$x >= min  ,]
+  
+  if(nrow(userData) ==0 ) return (NULL)
+  
+  
+  statMean <<- round(mean(userData$x),digits = 2)
+  statMedian <<- median(userData$x)
+  statMin <<- min(userData$x)
+  statMax <<- max(userData$x)
+}
 # getTransposeData<-function (userList){
 #   data<- getUserData(userList)
 #   data<- as.data.frame(t(data))
